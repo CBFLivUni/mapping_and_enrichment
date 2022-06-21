d18b3be1182418bad1053a0e7ec331ea51b4d58f
Simon Perkins <simon.richard.perkins@googlemail.com>
2022-06-21 06:52:33 +0100

    Partially fixed bug with db_ortho web call. Still broken for very large calls.

--- mapping_with_db_ortho.R
+++ mapping_with_db_ortho.R
@@ -4,6 +4,9 @@ library(glue)
 # Please see https://biodbnet-abcc.ncifcrf.gov/db/dbOrtho.php for an idea of what the input_id and output_id could be.
 # If you supply too many input values you may get a HTTP error - try splitting it up into multiple calls if that happens.
 map_using_db_ortho <- function(input_id, input_values, input_taxon, output_taxon, output_id, output_format = 'row') {
-  url_ = glue('https://biodbnet-abcc.ncifcrf.gov/webServices/rest.php/biodbnetRestApi.json?method=dbortho&input={input_id}&inputValues={paste(input_values, collapse=",",sep="")}&inputTaxon={input_taxon}&outputTaxon={output_taxon}&output={output_id}&format={output_format}')
-  fromJSON(url(url_))
+  url_ = URLencode(glue('https://biodbnet-abcc.ncifcrf.gov/webServices/rest.php/biodbnetRestApi.json?method=dbortho&input={input_id}&inputValues={paste(input_values, collapse=",",sep="")}&inputTaxon={input_taxon}&outputTaxon={output_taxon}&output={output_id}&format={output_format}'))
+  #js = url(url_)
+  #fromJSON(js)
+  res_ = rjson::fromJSON(file=paste(url_))
+  as_tibble(do.call(rbind, lapply(res_, as.data.frame)))
 }
